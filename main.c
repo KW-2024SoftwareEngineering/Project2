e5dbfdd497b050707199ffda6a4f2d8b24239f9c
seong hyeon Jeong <swjsh19@gmail.com>
2024-05-06 21:24:23 +0900

    main.c

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <time.h>
- 
+
 struct stat stat1, stat2;
 struct tm *time1, *time2;
 
@@ -58,11 +58,47 @@ void blockcmp(){
 }
 
 //두 개의 파일 수정 날짜를 비교하는 함수 작성
-void datecmp(){
-    
+void datecmp() {
+    int mon1, mon2, day1, day2;
+
+    mon1 = time1->tm_mon + 1;
+    mon2 = time2->tm_mon + 1;
+    day1 = time1->tm_mday;
+    day2 = time2->tm_mday;
+
+    printf("date compare\n");
+    if (mon1 > mon2) {
+        printf("text2 is early\n\n");
+    }
+    else if (mon1 < mon2) {
+        printf("text1 is early\n\n");
+    }
+    else {
+        if (day1 > day2) {
+            printf("text2 is early\n\n");
+        }
+        else if (day1 < day2) {
+            printf("text1 is early\n\n");
+        }
+        else {
+            printf("same date\n\n");
+        }
+    }
 }
 
 //두 개의 파일 수정 시간을 비교하는 함수 작성
-void timecmp(){
-    
+void timecmp() {
+    int time_1 = time1->tm_hour * 60 + time1->tm_min;
+    int time_2 = time2->tm_hour * 60 + time2->tm_min;
+
+    printf("time compare\n");
+    if (time_1 > time_2) {
+        printf("text2 is early\n\n");
+    }
+    else if (time_2 > time_1) {
+        printf("text1 is early\n\n");
+    }
+    else {
+        printf("same time\n\n");
+    }
 }
